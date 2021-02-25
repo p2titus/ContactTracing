914974ff43309e89b598c863b72fca5061dd6128
Mike <michael.hallam3@gmail.com>
2021-02-25 20:53:34 +0000

    added further fields to database based off suggestions

--- ContactTracing/models.py
+++ ContactTracing/models.py
@@ -13,13 +13,17 @@ class Addresses(models.Model):
     addr = models.CharField(max_length=256)
     postcode = models.CharField(max_length=8)
     # max length assumed from https://ideal-postcodes.co.uk/guides/uk-postcode-format
+    latitude = models.FloatField
+    longitude = models.FloatField
 
 
 class People(models.Model):
     fname = models.CharField(max_length=32)
     lname = models.CharField(max_length=32)
+    age = models.IntegerField
     location = models.ForeignKey(Addresses)
     phone_num = models.CharField(max_length=13)
+    email = models.EmailField
     # allows for country code (e.g. +44)
 
 
