e480c7c766e96713beff97f5ee057a8a8fcc9d7f
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2021-03-03 18:09:42 +0000

    Add data for different types of regions of UK, code to load it to database

---
+++ government/management/commands/area_load.py
@@ -0,0 +1,33 @@
+from django.core.management.base import BaseCommand, CommandError
+from django.contrib.gis.gdal import DataSource
+from django.contrib.gis.utils import LayerMapping
+from government.models import Area
+
+to_load = [("./government/areas/1countries/Countries_(December_2019)_Boundaries_UK_BGC.shp", Area.COUNTRY, "ctry19nm"),
+           ("./government/areas/2regions/Regions_(December_2019)_Boundaries_EN_BGC.shp", Area.REGION, "rgn19nm"),
+           ("./government/areas/3counties/Counties_and_Unitary_Authorities_(December_2019)_Boundaries_UK_BGC.shp", Area.COUNTY, "ctyua19nm"),
+           ("./government/areas/4local_authority_districts/Local_Authority_Districts_(December_2020)_UK_BGC.shp", Area.LA, "LAD20NM")]
+class CustomLayerMapping(LayerMapping):
+    def __init__(self, *args, **kwargs):
+        self.custom = kwargs.pop('custom', {})
+        super(CustomLayerMapping, self).__init__(*args, **kwargs)
+
+    def feature_kwargs(self, feature):
+        kwargs = super(CustomLayerMapping, self).feature_kwargs(feature)
+        kwargs.update(self.custom)
+        return kwargs
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        # ignores all options/args
+        for (file, t, name_field ) in to_load:
+            self.stdout.write(" %s, %s, %s" % (file, t, name_field))
+            ds = DataSource(file)
+            layer = ds[0]
+            mapping = {
+                "poly": "POLYGON",
+                "name": name_field,
+
+            }
+            lm = CustomLayerMapping(model=Area,data= ds,mapping= mapping, custom= {"type": t})
+            lm.save(verbose=False, strict=True)
--- government/models.py
+++ government/models.py
@@ -1,3 +1,18 @@
-from django.db import models
 
-# Create your models here.
+from django.contrib.gis.db import models
+from django.contrib.gis.geos import Point
+
+class Area(models.Model):
+    COUNTRY = "CTRY"
+    REGION = "REG"
+    COUNTY = "CNTY"
+    LA = "LA"
+    AREA_TYPES = (
+        (COUNTRY, "Country"),
+        (REGION, "Region"),
+        (COUNTY, "County/Unitary Authority"),
+        (LA, "Local Authority District")
+    )
+    name = models.CharField(max_length=256)
+    poly = models.MultiPolygonField()
+    type = models.CharField(max_length=4, choices=AREA_TYPES, default=LA)
