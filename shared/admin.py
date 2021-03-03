56609280bbd2ecd62eb64eca0d5cdee554a443b1
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2021-03-03 16:09:22 +0000

    Couple more minor changes

--- shared/admin.py
+++ shared/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Addresses, Contact, ContactContacted, People, TestContacted, Test
 # Register your models here.
+# TODO: remove these before production (these allow for basic editing/adding of data)
+admin.site.register(Addresses)
+admin.site.register(Contact)
+admin.site.register(ContactContacted)
+admin.site.register(People)
+admin.site.register(TestContacted)
+admin.site.register(Test)
