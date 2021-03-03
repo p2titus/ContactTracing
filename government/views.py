894883c896e87c23d2cf1bb5ceb11c342f949faa
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2021-03-03 16:39:51 +0000

    Add a basic template to start off with

--- government/views.py
+++ government/views.py
@@ -1,4 +1,7 @@
 from django.http import HttpResponse
+from django.template import loader
+
 
 def index(request):
-    return HttpResponse("Hello, Government user!.")
+    template = loader.get_template("government/index.html")
+    return HttpResponse(template.render({}, request))
