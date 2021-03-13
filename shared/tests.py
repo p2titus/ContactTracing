8a5cceb12e002ede1f87c2e78c0b1d340fe7bde4
Mike <michael.hallam3@gmail.com>
2021-03-13 12:10:13 +0000

    wrote some basic stests for models (currently fail due to missing columns etc)

--- shared/tests.py
+++ shared/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+# tests here are primarily to test the models created.
+# these should pass by default. they may be moved to another folder later
+
+from django.test import TestCase
+from .models import *
+
+
+class ModelsTests(TestCase):
+    def setup(self):
+        self.create_addresses()
+        self.create_people()
+
+    def create_people(self):
+        People.object.create(fname="Person", lname="A", addr=1, phone_num="A")
+        People.object.create(fname="Person", lname="B", addr=2, phone_num="B")
+
+    def create_addresses(self):
+        Addresses.objects.create(addr="addrA", postcode="postcodeA")
+        Addresses.objects.create(addr="addrB", postcode="postcodeB")
+
+    def test_people_creation(self):
+        b = People.objects.get(lname="B")
+        self.assertEqual(b.phone_num, "B")
