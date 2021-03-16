a268576b0f4b692f29e1bbac00ae4f1e5ea139d5
Mike <michael.hallam3@gmail.com>
2021-03-16 20:20:19 +0000

    began to separate test database construction from actual tests being run on the database

--- shared/tests.py
+++ shared/tests.py
@@ -23,24 +23,6 @@ class ModelsTests(TestCase):
         x = self.create_test_inst()
         self.create_contact_inst(x)
 
-    def create_addresses(self):
-        Addresses.objects.create(addr="addrA", postcode="postcodeA")
-        Addresses.objects.create(addr="addrB", postcode="postcodeB")
-
-    def create_people(self):
-        addrA = Addresses.objects.get(addr="addrA")
-        addrB = Addresses.objects.get(addr="addrB")
-        a = People.objects.create(name="Person A", phone_num="A", email="A@example.com", location=addrA)
-        b = People.objects.create(name="Person B", phone_num="B", email="B@example.com", location=addrB)
-        return (a, b)
-
-    def create_test_inst(self):
-        return Test.objects.create(person=self._pos_case, result=True)
-
-    def create_contact_inst(self, test):
-        addr_b = Addresses.objects.get(addr="addrB")
-        Contact.objects.create(case_contact=self._contact, positive_case=test, location=addr_b)
-
     def test_people_creation(self):
         b = People.objects.get(email="B@example.com")
         self.assertEqual(b.phone_num, "B")
