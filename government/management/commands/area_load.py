6be40735d8b21b568611b5a83167215235ccddb7
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2021-03-17 11:48:10 +0000

    Switch to lower-resolution map data

--- government/management/commands/area_load.py
+++ government/management/commands/area_load.py
@@ -5,8 +5,8 @@ from government.models import Area
 
 to_load = [("./government/areas/1countries/Countries_(December_2019)_Boundaries_UK_BUC.shp", Area.COUNTRY, "ctry19nm"),
            ("./government/areas/2regions/Regions_(December_2019)_Boundaries_EN_BUC.shp", Area.REGION, "rgn19nm"),
-           ("./government/areas/3counties/Counties_and_Unitary_Authorities_(December_2019)_Boundaries_UK_BGC.shp", Area.COUNTY, "ctyua19nm"),
-           ("./government/areas/4local_authority_districts/Local_Authority_Districts_(December_2020)_UK_BGC.shp", Area.LA, "LAD20NM")]
+           ("./government/areas/3counties/Counties_and_Unitary_Authorities_(December_2019)_Boundaries_UK_BUC.shp", Area.COUNTY, "ctyua19nm"),
+           ("./government/areas/4local_authority_districts/Local_Authority_Districts_(December_2020)_UK_BUC.shp", Area.LA, "LAD20NM")]
 class CustomLayerMapping(LayerMapping):
     def __init__(self, *args, **kwargs):
         self.custom = kwargs.pop('custom', {})
