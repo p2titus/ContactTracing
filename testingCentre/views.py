4570697ddcaa5eb219a9968f04d031ceb30d45b0
Sian <siani.saul@gmail.com>
2021-03-21 15:13:59 +0000

    first attempt at getting form data into database, does not currently work

--- testingCentre/views.py
+++ testingCentre/views.py
@@ -4,6 +4,8 @@ from django.http import HttpResponseRedirect, HttpResponse
 
 from .forms import SingleTestForm
 
+from shared.models import People, Addresses, Test
+
 # Create your views here.
 class LoginView(generic.TemplateView):
     pass
@@ -13,6 +15,45 @@ class ChooseInputMethodView(generic.TemplateView):
 
 class InputCSVView(generic.TemplateView):
     template_name = "testingCentre/csv.html"
+
+# find if a person already exists in the database. Enter them if not and return the pk.
+def lookup_person(name, phone, age, email, address, postcode):
+    person = People.objects.filter(name__exact=name, phone_num__exact=phone, email__exact=email, age__exact=age, location__addr__exact=address, location__postcode__exact=postcode)
+    assert(person.count <= 1)
+    if person.count == 0:
+        # add person to model
+        # check if address exists
+        possibleAddress = Addresses.objects.filter(addr__exact=address, postcode=postcode)
+        if address.count == 0:
+            p = People(
+                name= name,
+                phone_num=phone,
+                email= email,
+                location= Addresses(addr=address, postcode=postcode)
+            )
+        else:
+            p = People(
+                name= name,
+                phone_num=phone,
+                email= email,
+                location= possibleAddress.first.pk
+            )
+        p.save()
+        return p.pk
+    else:
+        return person.first.pk
+
+# assumes that the test has not already been input
+def input_test(personID, date, res):
+    t = Test(
+        person = personID,
+        test_date = date,
+        result = res
+    )
+    t.save()
+
+def send_text(phone, res):
+    pass
 
 def thanks(request):
     return HttpResponse("Result Entered")
@@ -25,7 +66,17 @@ def InputSingleTest(request):
         # check whether it's valid:
         if form.is_valid():
             # process the data in form.cleaned_data as required
-            # ...
+            person = lookup_person(form.cleaned_data['name'],
+                                   form.cleaned_data['phone_num'],
+                                   form.cleaned_data['age'],
+                                   form.cleaned_data['email'],
+                                   form.cleaned_data['addr'],
+                                   form.cleaned_data['postcode'])
+            input_test(person,
+                       form.cleaned_data['test_date'],
+                       form.cleaned_data['result'])
+            send_text(form.cleaned_data['phone'],
+                      form.cleaned_data['result'])
             # redirect to a new URL:
             return HttpResponseRedirect('thanks/')
 
@@ -34,20 +85,3 @@ def InputSingleTest(request):
         form = SingleTestForm()
 
     return render(request, 'testingCentre/singleTest.html', {'form': form})
-
-# from shared.models import People, Addresses, Test
-# # find if a person already exists in the database. Enter them if not and return the pk.
-# def lookup_person(name, phone, email, address, postcode):
-#     pass
-#
-# # assumes that the test has not already been input
-# def input_test(personID, date, res):
-#     t = Test(
-#         person = personID,
-#         test_date = date,
-#         result = res
-#     )
-#     t.save()
-#
-# def send_text(phone, res):
-#     pass
