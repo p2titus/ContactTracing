47f684f82aaf096ee296281ca3245caea9ebe350
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2021-03-27 16:58:34 +0000

    Add layout, styling for cases & tests, with mock/random data - Has headline statistics, graphs, placeholders for area data, in table & map form - Loads the favicon, basic navigation bar - Still todo: use actual data, fill in footer, add other links to top bar, replace useless search field with a login button

--- government/views.py
+++ government/views.py
@@ -4,18 +4,77 @@ from django.db.models import Count, Max, Min, Avg
 from shared.models import People, Test, TestContacted, ContactContacted, Contact
 from datetime import date, time, datetime, timedelta
 
+
 def index(request):
     return HttpResponse("Hello, Government user!.")
 
+
+pretty_times = {
+    1: "the last day",
+    7: "the last week",
+    14: "the last fortnight",
+    30: "the last month",
+    365: "the last year"
+}
+
+
+def get_time_description(time_frame, prev=False):
+    if time_frame in pretty_times.keys():
+        t = pretty_times[time_frame]
+    else:
+        if time_frame % 365 == 0:
+            t = "the last %d years" % (time_frame // 365)
+        if time_frame % 30 == 0:
+            t = "the last %d months" % (time_frame // 30)
+        if time_frame % 7 == 0:
+            t = "the last %d weeks" % (time_frame // 7)
+        if time_frame >= 2:
+            t = "the last %d days" % time_frame
+        else:  # i.e <= 0
+            t = "invalid time range"
+    if prev:
+        return t.replace(" last ", " preceding ")
+    else:
+        return t
+
+
 def timebased(request, time_frame):
     time_threshold = date.today() - timedelta(days=time_frame)
-    testObjects = Test.objects.filter(test_date__gt=time_threshold)
-    numTests = testObjects.count()
-    posTests = testObjects.filter(result=True)
-    numPos = posTests.count()
-#    posRate = numPos / numTests
-    peopleObjects = People.objects.filter(test__test_date__gt=time_threshold)
-    people_by_Contacts = peopleObjects.annotate(Count('contact'))
-    max_contacts = people_by_Contacts.aggregate(max=Max('contact__count'))['max']
-    avg_contacts = people_by_Contacts.aggregate(average=Avg('contact__count'))['average']
-    return render(request, 'government/timebased.html', {'numTests':numTests, 'numPos': numPos, 'max_contacts': max_contacts, 'avg_contacts': avg_contacts, 'time_frame':time_frame})
+    test_objects = Test.objects.filter(test_date__gt=time_threshold)
+    num_tests = test_objects.count()
+    pos_tests = test_objects.filter(result=True)
+    num_pos = pos_tests.count()
+
+    if num_tests == 0:
+        pos_rate = "NaN"
+    else:
+        pos_rate = num_pos / num_tests
+
+    people_objects = People.objects.filter(test__test_date__gt=time_threshold)
+    people_by_contacts = people_objects.annotate(Count('contact'))
+    max_contacts = people_by_contacts.aggregate(max=Max('contact__count'))['max']
+    avg_contacts = people_by_contacts.aggregate(average=Avg('contact__count'))['average']
+    return render(request, 'government/timebased.html',
+                  {
+                      # Data for this time frame
+                      'num_tests': num_tests,
+                      'num_pos': num_pos,
+                      'max_contacts': max_contacts,
+                      'avg_contacts': avg_contacts,
+                      'pos_rate': pos_rate,
+                      # Change over previous time frame
+                      'change_num_tests': 1234,
+                      'change_num_pos': -1234,
+                      'change_max_contacts': -2,
+                      'change_avg_contacts': 1,
+                      'change_pos_rate': 1.45,
+                      # Time information
+                      'time_frame': time_frame,
+                      'time_description': get_time_description(time_frame),
+                      'prev_time_description': get_time_description(time_frame, True),
+                      'days_range': [time_threshold + timedelta(days=x) for x in range(time_frame)],
+                      # Charting information
+                      'charts_data': [(None, "Positive cases", "pos_case_canvas"),
+                                      (None, "Tests performed", "num_tests_canvas"),
+                                      (None, "Positivity rate", "pos_rate_canvas")]
+                  })
