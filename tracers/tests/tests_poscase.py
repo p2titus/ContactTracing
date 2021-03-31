0b4eb089fdc0e63e8f96e1b9b0dc21822830c043
autay27 <autay27@gmail.com>
2021-03-31 15:15:55 +0100

    added tests for claims + fixed claims system

--- tracers/tests/tests_poscase.py
+++ tracers/tests/tests_poscase.py
@@ -71,6 +71,53 @@ class GetTestsInSequence(TestCase):
         self.assertEqual(views_help.next_test(),
                          toyExpectedNext(self.datalist))
 
+class ClaimExpires(TestCase):
+    datalist = [("Jane Doe", True, False),
+                ("John Doe", True, False),
+                ("Joe Bloggs", True, False)]
+
+    def setUp(self):
+        loadToyData(self.datalist)
+
+    def test_claim_expires(self):
+        t1 = views_help.next_test()
+
+        # simulate passage of time by manually changing contact_start field to 'a long time in the past'
+        t1.contact_start -= views_help.expiry_time * 2
+        t1.save(update_fields=["contact_start"])
+
+        t2 = views_help.next_test()
+        self.assertEqual(t1, t2)
+
+class ClaimCanBeRefreshed(TestCase):
+    datalist = [("Jane Doe", True, False)]
+
+    def setUp(self):
+        loadToyData(self.datalist)
+
+    def test_claim_refreshed(self):
+        t1 = views_help.next_test()
+        orig_time = t1.contact_start
+        self.assertTrue(views_help.refresh_claim(t1))
+        self.assertGreater(t1.contact_start, orig_time)
+
+class ExpiredClaimCantBeRefreshed(TestCase):
+    datalist = [("Jane Doe", True, False)]
+
+    def setUp(self):
+        loadToyData(self.datalist)
+
+    def test_cant_be_refreshed(self):
+        t1 = views_help.next_test()
+
+        # simulate passage of time with manual update to contact_start field
+        t1.contact_start -= views_help.expiry_time * 2
+        t1.save(update_fields=["contact_start"])
+
+        self.assertFalse(views_help.refresh_claim(t1))
+
+
+# Figure out the "naive datetime" error
+#I feel like I haven't explored the issue of time travel/dates in the future
 #expiring claims needs to be tested
 # change after adding new contacted entries needs to be tested
-# concurrency stuff needs to be tested
--- tracers/views_help.py
+++ tracers/views_help.py
@@ -1,20 +1,23 @@
 from shared.models import Test, TestContacted
 from django.db import transaction
 import datetime
-from datetime import timedelta
+from datetime import timezone, timedelta
 from django.db.models import Q
 
+# time after which claim expires
+expiry_time = timedelta(hours=1)
 
-# claims & returns earliest positive test that hasn't been claimed
-# a claim will eventually expire
+# for reference, in this datetime representation, future > past
+
+
 def tests_needing_contacting():
-    expiry_time = timedelta(hours=1)
-    claim_requirement = Q(being_contacted__exact=True) & Q(contact_start__gt=datetime.datetime.now() - expiry_time)
-    return Test.objects.exclude(claim_requirement) \
+    unexpired_claim = Q(being_contacted__exact=True) & Q(contact_start__gt=datetime.datetime.now(timezone.utc) - expiry_time)
+    return Test.objects.exclude(unexpired_claim) \
         .exclude(person__in=TestContacted.objects.values_list('case', flat=True)) \
         .filter(result__exact=True)
 
 
+# claims & returns earliest positive test that hasn't been claimed
 def next_test():
     try:
         with transaction.atomic():
@@ -24,3 +27,17 @@ def next_test():
     except Test.DoesNotExist:
         test = None
     return test
+
+
+# updates timestamp on (un-expired) claim so it doesn't expire
+# returns true for successful update
+def refresh_claim(psn):
+    with transaction.atomic():
+        now = datetime.datetime.now(timezone.utc)
+        if psn.being_contacted & (psn.contact_start > now - expiry_time):
+            psn.contact_start = now
+            psn.save(update_fields=["contact_start"])
+            return True
+        else:
+            return False
+
