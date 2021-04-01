68f86ffaf3c239de6ffd5152a53635f68b1b8673
jamiellagann <79455155+jamiellagann@users.noreply.github.com>
2021-04-01 20:31:02 +0100

    merged some of mike's updates in main (idk if that's something that committing will achieve) moved functionality for obtaining time-based data to a specific function, rather than to the view itself. Allows us to more easily compare data.

--- government/views.py
+++ government/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Count, Max, Min, Avg
 from shared.models import People, Test, TestContacted, ContactContacted, Contact
-from datetime import date, time, datetime, timedelta
+from datetime import date, datetime, timedelta
 
 
 def index(request):
@@ -37,10 +37,11 @@ def get_time_description(time_frame, prev=False):
     else:
         return t
 
-
-def timebased(request, time_frame):
-    time_threshold = date.today() - timedelta(days=time_frame)
-    test_objects = Test.objects.filter(test_date__gt=time_threshold)
+def get_time_statistics(time_frame,offset):
+    #i.e. say we want the aggregate data over a week, 4 weeks ago, we would call gts(7,3). zero indexed, i.e. gts(7,0) is the most recent week of stats
+    time_threshold_0 = date.today() - timedelta(days=time_frame*(offset+1))
+    time_threshold_1 = date.today() - timedelta(days=offset*time_frame)
+    test_objects = Test.objects.filter(test_date__gt=time_threshold_0).filter(test_date__lte=time_threshold_1)
     num_tests = test_objects.count()
     pos_tests = test_objects.filter(result=True)
     num_pos = pos_tests.count()
@@ -50,24 +51,45 @@ def timebased(request, time_frame):
     else:
         pos_rate = 100 * num_pos / num_tests
 
-    people_objects = People.objects.filter(test__test_date__gt=time_threshold)
+    people_objects = People.objects.filter(test__test_date__gt=time_threshold_0).filter(test__test_date__lte=time_threshold_1)
     people_by_contacts = people_objects.annotate(Count('contact'))
+
+    age_groups = []
+    for i in range (0,90,10):
+        birthday = date.today() - timedelta(days=i*365)
+        age_groups.append(birthday)
+
+    age_data = []
+    for i in range(8,-1,-1):
+        birthday = age_groups[i]
+        age_data.append(people_objects.filter(date_of_birth__gte=birthday).count())
+        if i<8:
+            age_data[i] -= age_data[i+1]
+
     max_contacts = people_by_contacts.aggregate(max=Max('contact__count'))['max']
     avg_contacts = people_by_contacts.aggregate(average=Avg('contact__count'))['average']
+    return {'max': max_contacts, 'avg': avg_contacts, 'age_data': age_data, 'num' : num_tests, 'pos' : pos_tests, 'rate' : pos_rate }
+
+def timebased(request, time_frame):
+
+    sn = get_time_statistics(time_frame, 0) #stats now, i.e. in the last time_frame days
+    sp = get_time_statistics(time_frame,1) # stats then, i.e. in the time_frame days before the start of the most recent time_frame
+    time_threshold = date.today() - timedelta(days=time_frame)
+
     return render(request, 'government/timebased.html',
                   {
                       # Data for this time frame
-                      'num_tests': num_tests,
-                      'num_pos': num_pos,
-                      'max_contacts': max_contacts,
-                      'avg_contacts': avg_contacts,
-                      'pos_rate': pos_rate,
+                      'num_tests': sn['num'],
+                      'num_pos': sn['pos'],
+                      'max_contacts': sn['max'],
+                      'avg_contacts': sn['avg'],
+                      'pos_rate': sn['rate'],
                       # Data for previous time frame
-                      'prev_num_tests': 1234,
-                      'prev_num_pos': -1234,
-                      'prev_max_contacts': -2,
-                      'prev_avg_contacts': 1,
-                      'prev_pos_rate': 1.45,
+                      'prev_num_tests': sp['num'],
+                      'prev_num_pos': sp['pos'],
+                      'prev_max_contacts': sp['max'],
+                      'prev_avg_contacts': sp['avg'],
+                      'prev_pos_rate': sp['rate'],
                       # Time information
                       'time_frame': time_frame,
                       'time_description': get_time_description(time_frame),
