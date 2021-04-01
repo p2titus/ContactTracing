aa2dde03d341edc026beaa8078837ff2c43b863b
Max Cairney-Leeming <max@cairneyleeming.co.uk>
2021-04-01 17:53:36 +0100

    Add layout/styling for contacts data, but only placeholder stuff for now.

--- government/views.py
+++ government/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.db.models import Count, Max, Min, Avg
 from shared.models import People, Test, TestContacted, ContactContacted, Contact
 from datetime import date, time, datetime, timedelta
-
+from government.hooks.dbscan import MIN_PTS,EPS
 
 def index(request):
     return HttpResponse("Hello, Government user!.")
@@ -53,7 +53,11 @@ def timebased(request, time_frame):
     people_objects = People.objects.filter(test__test_date__gt=time_threshold)
     people_by_contacts = people_objects.annotate(Count('contact'))
     max_contacts = people_by_contacts.aggregate(max=Max('contact__count'))['max']
+    if max_contacts is None:
+        max_contacts = 0
     avg_contacts = people_by_contacts.aggregate(average=Avg('contact__count'))['average']
+    if avg_contacts is None:
+        avg_contacts = 0
     return render(request, 'government/timebased.html',
                   {
                       # Data for this time frame
@@ -62,12 +66,14 @@ def timebased(request, time_frame):
                       'max_contacts': max_contacts,
                       'avg_contacts': avg_contacts,
                       'pos_rate': pos_rate,
+                      'cases_contacted': 78.45,
                       # Data for previous time frame
                       'prev_num_tests': 1234,
                       'prev_num_pos': -1234,
                       'prev_max_contacts': -2,
                       'prev_avg_contacts': 1,
                       'prev_pos_rate': 1.45,
+                      'prev_cases_contacted': 96.34,
                       # Time information
                       'time_frame': time_frame,
                       'time_description': get_time_description(time_frame),
@@ -76,5 +82,8 @@ def timebased(request, time_frame):
                       # Charting information
                       'charts_data': [(None, "Positive cases", "pos_case_canvas"),
                                       (None, "Tests performed", "num_tests_canvas"),
-                                      (None, "Positivity rate", "pos_rate_canvas")]
+                                      (None, "Positivity rate", "pos_rate_canvas")],
+                      # Clustering parameters
+                      'min_cases_in_cluster': MIN_PTS,
+                      'cluster_radius': EPS
                   })
