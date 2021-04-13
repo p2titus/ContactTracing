b5c2d02a0e30226bd35b7e33fa9271755855d220
Mike <michael.hallam3@gmail.com>
2021-04-13 20:50:56 +0100

    began implementing rabbitmq to allow for receipt and sending of pos cases to tracers

---
+++ tracer_queues/main.py
@@ -0,0 +1,35 @@
+import pika
+import json
+
+
+# essentially macros
+POS_CASE = 'pos_case'
+IN_CONTACT = 'in_contact'
+PARAM_FILE = 'param.json'
+
+
+def main():
+    params = parse()
+    chan, con = setup(params)
+
+
+def setup(params: dict) -> (pika.connection.channel.Channel, str):
+    global POS_CASE
+    global IN_CONTACT
+    host = params['host']
+    con = pika.BlockingConnection(pika.ConnectionParameters(host))
+    chan = con.channel()
+    chan.queue_declare(queue=POS_CASE)
+    chan.queue_declare(queue=IN_CONTACT)
+    return chan, con
+
+
+def parse():
+    global PARAM_FILE
+    with open(PARAM_FILE) as f:
+        data = json.load(f)
+    return data
+
+
+if __name__ == '__main__':
+    main()
