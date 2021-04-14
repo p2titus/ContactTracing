7e615c39eefa98bcb1fb91b8c2398983f97d0d34
Mike <michael.hallam3@gmail.com>
2021-04-14 19:31:05 +0100

    added extract_case_data method to allow data for form to be mapped to dict

--- testingCentre/views.py
+++ testingCentre/views.py
@@ -18,13 +18,40 @@ def thanks(request):
     return HttpResponse("Result Entered")
 
 def add_person(request):
+    print('\nstart\n')
     if request.method == 'POST':
         form = SingleTestForm(request.POST)
+        print('form put in var')
         if form.is_valid():
+            print('\na\n')
+            import tracer_queues
+            case = extract_case_data(form)
+            print('\nflag\n')
+            if case is not None:
+                tracer_queues.main.add_poscase(case)
             form.add_person()
+
             return HttpResponseRedirect('/testingCentre/singleTest/thanks/')
+        else:
+            print('fail')
+            return None
     else:
+        print('\nb\n')
         return HttpResponseRedirect('/testingCentre/singleTest/')
+
+
+def extract_case_data(form):
+    x = None
+    if form.cleaned_data['result'] is True:
+        x = {
+            'name': form.cleaned_data['name'],
+            'phone_num': form.cleaned_data['phone_num'],
+            'date_of_birth': form.cleaned_data['date_of_birth'],
+            'email': form.cleaned_data['email'],
+            'test_date': form.cleaned_data['test_date']
+        }
+    return x
+
 
 class InputSingleTestView(generic.FormView):
     template_name = 'testingCentre/singleTest.html'
