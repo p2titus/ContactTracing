159344b69ef078fba919551ef332b9b391c89c94
Mike <michael.hallam3@gmail.com>
2021-04-14 11:34:50 +0100

    added ability to start rabbitmq server (permissions for bash script will need to be set correctly)

--- tracer_queues/main.py
+++ tracer_queues/main.py
@@ -6,16 +6,31 @@ import json
 POS_CASE = 'pos_case'
 IN_CONTACT = 'in_contact'
 PARAM_FILE = 'param.json'
+RABBITMQ_STARTUP_SCRIPT = 'start_rabbitmq.sh'
 
 
 def main():
     params = parse()
-    chan, con = setup(params)
+    start_server(params)
+    chan, con = setup(params=params)
+    print('queues created')
+    input('press enter to close this connection to rabbit-mq gracefully')
+    con.close()
+
+
+def start_server(params):
+    default_port = 25672
+    import socket
+    # check to see if server listening on default port
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        server_active = s.connect_ex(params['host'], default_port)
+    # start server via bash script if it isn't already running
+    if not server_active:
+        import subprocess
+        subprocess.call(RABBITMQ_STARTUP_SCRIPT)
 
 
 def setup(params: dict) -> (pika.connection.channel.Channel, str):
-    global POS_CASE
-    global IN_CONTACT
     host = params['host']
     con = pika.BlockingConnection(pika.ConnectionParameters(host))
     chan = con.channel()
---
+++ tracer_queues/start_rabbitmq.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+# rabbitmq requires root permissions to run; this will work for now
+# pre: port should not be in use
+sudo rabbitmq-server
