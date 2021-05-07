a2ee2d5588dbee29e1c40e77f8209207d61bbe48
Mike <michael.hallam3@gmail.com>
2021-05-07 18:24:11 +0100

    rewrote rabbitmq code so it works better

--- tracer_queues/main.py
+++ tracer_queues/main.py
@@ -5,6 +5,7 @@ import json
 
 
 # essentially macros
+EXCHANGE_NAME = 'cases'
 POS_CASE = 'pos_case'
 IN_CONTACT = 'in_contact'
 PARAM_FILE = 'param.json'
@@ -13,64 +14,73 @@ HOSTNAME = 'localhost'  # default value given for testing
 PORT = 25672
 PERSISTENT = 2
 
+"""
+acts as a wrapper around rabbitmq
+running the file will create the pre-requisite exchanges needed
+"""
 
+
+# pre: rabbitmq instance required to be running already
+# declares the required queues to be used for retrieval and sending of pos cases/contacts to be contacted
 def run_server_instance():
-    params = parse()
-    if params is not None:
-        global HOSTNAME
-        HOSTNAME = params['host']
-    start_server(params)
-    chan, con = setup(params=params)
-    print('queues created')
-    input('press enter to close this connection to rabbit-mq gracefully\n')
-    con.close()
+    global HOSTNAME
+
+    params = parse(PARAM_FILE)
+    HOSTNAME = params['host']
+    setup_queues(params)
 
 
-def start_server(params=None):
-    '''if params is None:
-        params = {'host': HOSTNAME}
-    import socket
-    # check to see if server listening on default port
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        server_active = s.connect_ex((params['host'], PORT))
-    # start server via bash script if it isn't already running
-    if not server_active:
-        import subprocess
-        subprocess.call(RABBITMQ_STARTUP_SCRIPT)'''
-    pass  # TODO - get this working
-
-
-def setup(params: dict):
-    if params is None:
-        host = HOSTNAME
-    else:
-        host = params['host']
-    con = pika.BlockingConnection(pika.ConnectionParameters(host))
+def setup_queues(params):
+    host = params['host']
+    con = pika.BlockingConnection(pika.ConnectionParameters(host=host))
     chan = con.channel()
-    chan.queue_declare(queue=POS_CASE, durable=True)
-    chan.queue_declare(queue=IN_CONTACT, durable=True)
-    chan.basic_qos(prefetch_count=1)
+
+    qs = [POS_CASE, IN_CONTACT]
+
+    for q in qs:
+        chan.queue_declare(queue=q, durable=True)
+
+    return con, chan
+
+
+def parse(param_file):
+    try:
+        with open(param_file) as f:
+            params = json.load(f)
+    except IOError:
+        params = {'host': HOSTNAME}
+    if 'host' not in params:
+        params.update({'host': HOSTNAME})
+
+    return params
+
+
+def __setup(qname):
+    con = pika.BlockingConnection(pika.ConnectionParameters(host=HOSTNAME))
+    chan = con.channel()
+    chan.queue_declare(queue=qname, durable=True)
     return chan, con
 
 
-def parse():
-    import os
-    if os.path.isfile(PARAM_FILE):
-        with open(PARAM_FILE) as f:
-            data = json.load(f)
-    else:
-        data = None
-    return data
-
-
-# used to add positive cases to the queue
-async def add_poscase(case):
-    chan, con = setup(None)
+# used to add contacts/positive cases to the contact tracing queue
+def __add_to_queue(qname, case):
+    chan, con = __setup(qname)
     dte = DateTimeEncoder()
-    chan.basic_publish(exchange='', routing_key=POS_CASE, body=dte.encode(case),
+    chan.basic_publish(exchange='', routing_key=qname, body=dte.encode(case),
                        properties=pika.BasicProperties(
                            delivery_mode=PERSISTENT,
                        ))
+    con.close()
+
+
+# used to add contacts of positive cases to the queue
+def add_contact(contact):
+    __add_to_queue(IN_CONTACT, contact)
+
+
+# used to add positive cases to the queue
+def add_poscase(case):
+    __add_to_queue(POS_CASE, case)
 
 
 class DateTimeEncoder(json.JSONEncoder):
@@ -85,12 +95,14 @@ class DateTimeEncoder(json.JSONEncoder):
 
 def retrieve_pos_case():
     rp = RetrievePerson()
-    return rp.retrieve_person(POS_CASE)
+    chan, con = __setup(POS_CASE)
+    return rp.retrieve_person(POS_CASE, chan, con)
 
 
 def retrieve_contact():
     rp = RetrievePerson()
-    return rp.retrieve_person(IN_CONTACT)
+    chan, con = __setup(IN_CONTACT)
+    return rp.retrieve_person(IN_CONTACT, chan, con)
 
 
 class RetrievePerson:
@@ -98,7 +110,7 @@ class RetrievePerson:
 
     @staticmethod
     def __date_time_decoder(xs):
-        import dateutil
+        import dateutil.parser
 
         date_field_name = 'test_date'
 
@@ -106,29 +118,31 @@ class RetrievePerson:
             xs[date_field_name] = dateutil.parser.parse(xs[date_field_name])
             return xs
 
-    def retrieve_person(self, chan_name):
+    def retrieve_person(self, chan_name, chan, con):
 
         def callback(ch, method, properties, body):
             print('received %r' % body)
             self.__response = json.loads(body, object_hook=self.__date_time_decoder)
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
-        chan, con = setup(None)
+        # chan, con = __setup(chan_name)
 
         chan.basic_qos(prefetch_count=1)
-        chan.basic_consume(queue=chan_name, on_message_callback=callback)
+        chan.basic_consume(queue=chan_name, on_message_callback=callback, exclusive=True)
         chan.start_consuming()
 
         # with these figures, it checks the queue 20 times (once per millisecond) for a total of 20ms then continues
         # this parameter may need to be made longer to allow for more checks
         count = 0
-        max = 20
-        interval = 0.01  # 0.01s
+        max = 4
+        interval = 0.05  # 0.01s
 
         while self.__response is None and count < max:
             count += 1
             import time
             time.sleep(interval)
+
+        con.close()
 
         return self.__response
 
