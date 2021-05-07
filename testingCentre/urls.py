70824d25f7603b414379ca61a97bb25a71961c24
tianweicheng <terrycheng2001@gmail.com>
2021-05-07 15:00:49 +0800

    Updated interface design & Fixed a few bugs
    Updated interface design of testing centres site using Bootstrap CSS
    
    Additional changes:
    
    views.py: moved the json.load statement into the try block
    
    forms.py:
    
    included email address verification in the processing of the JSON file form
    
    fixed bug: test_date value entered via the form overridden by automatically added value (now) in the definition of class Test in shared/models.py
    
    A few other minor fixes

--- testingCentre/urls.py
+++ testingCentre/urls.py
@@ -10,13 +10,13 @@ urlpatterns = [
     # ex: /testingCentre/multipleTests/
     path('multipleTests/', views.InputMultipleTestsView.as_view(), name='multipleTests'),
     # /testingCentre/multipleTests/thanks/
-    path('multipleTests/thanks/', views.thanks, name='multipleTestsThanks'),
+    path('multipleTests/thanks/', views.ResultEnteredView.as_view(), name='multipleTestsThanks'),
     # /testingCentre/multipleTests/error/
     path('multipleTests/error/', views.DataFormatErrorView.as_view(), name='multipleTestsError'),
     # ex: /testingCentre/singleTest/
     path('singleTest/', views.InputSingleTestView.as_view(), name='singleTest'),
     # /testingCentre/thanks/
-    path('singleTest/thanks/', views.thanks, name='thanks'),
+    path('singleTest/thanks/', views.ResultEnteredView.as_view(), name='thanks'),
     # /testingCentre/singleTest/addPerson
     path('singleTest/addPerson/', views.add_person, name='add person'),
 
